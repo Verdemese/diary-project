cdeff8ac822aba735b542cee5187a59881cf7b0e
Verdemese <jaso26@hanmail.net>
2020-12-17 02:39:38 +0900

    modal을 추가하고, datesDetail의 오브젝트 구조를 변경함

--- src/container/CalendarBuilder/CalendarBuilder.js
+++ src/container/CalendarBuilder/CalendarBuilder.js
@@ -2,49 +2,83 @@ import React, { Component } from "react";
 //import styled from "styled-component";
 
 import Calendar from '../../component/Calendar/Calendar';
+import Modal from '../../component/UI/Modal/Modal';
+import WordContainer from '../../component/Calendar/WordsContainer/WordsContainer';
 
 class CalendarControl extends Component {
 
     state = {
-        dateOfFirstDay: '',
         datesDetail: [],
-        //    {
-                //1: {
-                //    checked: false,
-                //    dayOftheWeek: 'monday',
-                //    words: {
-                //        competition: '경쟁'
-                //    }
-                //}
-        //    },
-        //    {
-        //        2: {
-        //            checked: false,
-        //            dayOftheWeek: 'tuesday',
-        //            words: {
-        //                perpetual: '끊임없이 반복되는'
-        //            }
-        //        }
-        //    }
-        //],
-        datesOfMonth: 0,
-        thisMonth: 0
+        //{
+        //    checked: false,
+        //    dayOftheWeek: 'monday',
+        //    words: [{
+        //        competition: '경쟁'
+        //    }]
+        //}
+
+
+        //selectedYear
+        year: 0,
+        month: 0,
+        date: 0,
+        modalOpened: false,
+
     }
 
     componentDidMount() {
         const today = new Date();
+        const year = today.getFullYear();
+        const month = today.getMonth();
 
-        const thisMonth = today.getMonth();
-        const firstDay = new Date(today.getFullYear(), today.getMonth());
-        const lastDay = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+        const lastDay = new Date(year, month + 1, 0);
         const datesDetail = [];
-        const dateObj = {}
+        const dateContainer = {}
+        
+        for (let date = 1; date <= lastDay.getDate(); date++) {
 
-        for (let date = 1 ; date <= lastDay.getDate(); date++) {
+            const dayOfTheWeek = new Date(year, month, date);
 
-            const dayOfTheWeek = new Date(`${today.getFullYear()}-${today.getMonth()+1}-${date}`);
+            dateContainer[date] = {
+                date,
+                checked: date === today.getDate() ? true : false,
+                dayOfTheWeek: dayOfTheWeek.getDay(),
+                words: {
+                    competition: '경쟁'
+                }
+            }
 
-            dateObj[date] = {
+            datesDetail.push(dateContainer[date]);
+        }
+
+        this.setState({
+            today: today,
+            year: year,
+            month: month,
+            datesDetail: datesDetail,
+        });
+    }
+
+    nextMonthHandler = () => {
+        let year = this.state.year;
+        let month = this.state.month + 1; //현재의 month에서 1을 더함
+
+        //현재 state.month가 11인 상태에서 1을 더하여 11을 초과한 경우
+        //다음 해로 이동함과 동시에 월을 1월 1일로 초기화
+        if (month > 11) {
+            year = year + 1;
+            month = 0;
+        }
+
+        const lastDay = new Date(year, month + 1, 0);
+        const datesDetail = [];
+        const dateContainer = {}
+
+        for (let date = 1; date <= lastDay.getDate(); date++) {
+
+            const dayOfTheWeek = new Date(year, month, date);
+
+            dateContainer[date] = {
                 date,
                 checked: false,
                 dayOfTheWeek: dayOfTheWeek.getDay(),
@@ -53,23 +87,84 @@ class CalendarControl extends Component {
                 }
             }
 
-            datesDetail.push(dateObj[date]);
+            datesDetail.push(dateContainer[date]);
         }
 
-        this.setState({ 
-            datesOfMonth: lastDay, 
+        this.setState({
+            year: year,
+            month: month,
             datesDetail: datesDetail,
-            thisMonth: thisMonth
         });
+    }
+
+    previousMonthHandler = () => {
+        let year = this.state.year;
+        let month = this.state.month - 1;
+
+        //현재 state.month가 0인 상태에서 1을 빼서 0미만이 된 경우
+        //이전 해로 이동함과 동시에 월을 12월 1일로 초기화        
+        if (month < 0) {
+            year = year - 1;
+            month = 11;
+        }
+
+        const lastDay = new Date(year, month + 1, 0);
+        const datesDetail = [];
+        const dateContainer = {}
+
+        for (let date = 1; date <= lastDay.getDate(); date++) {
+
+            const dayOfTheWeek = new Date(year, month, date);
+
+            dateContainer[date] = {
+                date,
+                checked: false,
+                dayOfTheWeek: dayOfTheWeek.getDay(),
+                words: [{
+                    word: 'competition',
+                    meaning: '경쟁',
+                    id: 'competition경쟁'
+                }]
+            }
+
+            datesDetail.push(dateContainer[date]);
+        }
+
+        this.setState({
+            year: year,
+            month: month,
+            datesDetail: datesDetail,
+        });
+    }
+
+    selectMemoHandler = () => {
+        this.setState({ modalOpened: true });
+    }
+
+    closeModalHandler = () => {
+        this.setState({ modalOpened: false });
     }
 
     render() {
 
-        console.log(this.state.datesDetail);
+        const selectedMonth = `${this.state.year}-${this.state.month + 1}`;
+
+        console.log(this.state.modalOpened);
 
         return (
-            <Calendar 
-            datesDetail={this.state.datesDetail}/>
+            <>
+                <Modal 
+                    modalOpened={this.state.modalOpened}
+                    cancelModal={this.closeModalHandler}>
+                        <WordContainer />
+                    </Modal>
+                <Calendar
+                    selectedMemo={this.selectMemoHandler}
+                    nextMonth={this.nextMonthHandler}
+                    previousMonth={this.previousMonthHandler}
+                    datesDetail={this.state.datesDetail}
+                    selectedMonth={selectedMonth}/>
+            </>
         )
     }
 
