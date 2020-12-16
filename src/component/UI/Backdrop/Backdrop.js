dec56913b75ede6b8cf81e514a4001e157b8e68a
Verdemese <jaso26@hanmail.net>
2020-12-17 02:42:43 +0900

    modal과 backdrop을 추가함

---
+++ src/component/UI/Backdrop/Backdrop.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const Backdrop = styled.div`
+    position: fixed;
+    z-index: 499;
+    overflow: hidden;
+    width: 100vw;
+    height: 100vh;
+    visibility: hidden;
+    opacity: 0;
+    background-color: rgba(0, 0, 0, 0.3);
+    transition: opacity 0.3s;
+    
+    &.active {
+        visibility: visible;
+        opacity: 1;
+    }
+`
+
+const backdrop = props => {
+    return (
+        <Backdrop
+            className={props.opened ? 'active' : null}
+            onClick={props.clicked} />
+    )
+}
+
+export default backdrop;
---
+++ src/component/UI/Modal/Modal.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Backdrop from '../Backdrop/Backdrop';
+
+const Modal = styled.div`
+    visibility: hidden;
+    position: fixed;
+    z-index: 500;
+    left: 50%;
+    top: 30%;
+    transform: translateX(-50%);
+    box-shadow: 1px 1px 1px black;
+    width: 25rem;
+    height: 10rem;
+    margin: auto;
+    max-height: 30rem;
+    background-color: white;
+    opacity: 0;
+    transition: opacity 0.3s ease-out;
+
+    &.opened {
+        visibility: visible;
+        opacity: 1;
+    }
+`
+
+const modal = props => {
+    return (
+        <>
+            <Backdrop
+                opened={props.modalOpened}
+                clicked={props.cancelModal} />
+            <Modal className={props.modalOpened ? 'opened' : null}>
+                {props.children}
+            </Modal>
+            {/*<button
+                onClick={props.cancelModal}
+                type='button'
+                style={{
+                    backgroundColor: 'black',
+                    color: 'white',
+                    opacity: '0.3',
+                    borderRadius: '50px',
+                }}>X</button>*/}
+        </>
+    )
+}
+
+export default modal;
